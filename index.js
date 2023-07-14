619ad0f030e295f3d4651b4e97cb8775440947ab
Rae Stanton <raestanton93@gmail.com>
2023-07-13 18:00:37 -0700

    Calculator lab
    This utilizes functions that parse integers & floats if they are strings and adds simple arithmetic functions to calculate input.

---
+++ index.js
@@ -0,0 +1,29 @@
+function add(a, b) {
+  // we'll fill this in shortly
+  return a + b;
+};
+function subtract(c, d) {
+  return c - d;
+};
+function multiply(e, f) {
+  return e * f;
+};
+function divide(g, h) {
+  return g/h;
+};
+
+function increment(n) {
+  return n + 1;
+}
+
+function decrement(n) {
+  return n - 1;
+}
+
+function makeInt(string) {
+  return parseInt(string, 10);
+}
+
+function preserveDecimal(string) {
+  return parseFloat(string);
+}
